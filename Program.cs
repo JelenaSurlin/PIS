7fd171474172db7e35a81907fa766709c0b2c58c
lora4423 <147386886+lora4423@users.noreply.github.com>
2025-03-17 20:39:06 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ namespace ConsoleApp3._1
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Novo");
+            Console.WriteLine("Lora");
         }
     }
 }
